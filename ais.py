afec46d4b5084d93d241aa2d3cc9961cd4929a11
Werner Freund <wsfreund@gmail.com>
2021-04-21 17:34:33 -0300

    AIS can now be used with conditioned models

--- ais.py
+++ ais.py
@@ -62,7 +62,7 @@ class AIS(object):
     self._mcmc               = mcmc_type( self._energy_fn, **retrieve_kw(kw, 'mcmc_args',              {} ) )
     self._nchains            = retrieve_kw( kw, 'nchains', 16 )
 
-  def forward_ais(self, x, z = None, schedule = None):
+  def forward_ais(self, x, generator_input = None, schedule = None):
     """
     :param x: positions in the data input space
     :return: an array with a lower bound on logp for each x and its average
@@ -73,16 +73,29 @@ class AIS(object):
       schedule = self._scheduler()
     # initialize looping variables
     self._l_const_len_schedule = len(self._scheduler)
-    self._l_const_ndata        = x.shape[0]
-    self._l_const_data_dim     = x.shape[1:]
-    self._l_const_x            = tf.constant(x, dtype=tf.float32)
-    # We will work on flattened space until we get the final results.  Only
+    self._l_const_ndata        = self._generator_wrapper.get_batch_size_from_data( x )
+    self._l_const_x            = tf.constant( self._generator_wrapper.extract_target_space_from_data( x ), dtype=tf.float32)
+    # We work on flattened space until we get the final results. Only
     # exception is the observation model, which will use the matrix
     # representation in order to broadcast operations correctly in the input
     # space
-    self._l_z       = tf.Variable( self._generator_wrapper.sample_latent(
-        self._nchains*self._l_const_ndata
-      ) if z is None else z )
+    if generator_input is None:
+      # Generates the flattened space
+      generator_input = self._generator_wrapper.sample_generator_input( 
+          sampled_input = x
+        , n_samples = self._nchains*self._l_const_ndata
+      )
+      latent          = self._generator_wrapper.extract_latent_from_generator_input( generator_input )
+      condition       = self._generator_wrapper.extract_condition_from_generator_input( generator_input )
+    else:
+      # Assume generator_input state is unflattened around the batches and chains, as returned by AIS
+      latent          = self.flatten( self._generator_wrapper.extract_latent_from_generator_input( generator_input ) )
+      condition       = self._generator_wrapper.extract_condition_from_generator_input( generator_input )
+      condition       = tuple(self.flatten( c ) for c in condition ) if condition is not None else None
+    self._l_z       = tf.Variable( latent, dtype = tf.float32 )
+    # FIXME assumes that condition is a tuple/list, needs to implement method for dict
+    self._l_const_condition = tuple(tf.constant( c, dtype = tf.float32 ) for c in condition) if condition is not None else None
+    del generator_input, condition, latent
     self._l_w       = tf.Variable( tf.zeros( self._nchains*self._l_const_ndata, dtype = tf.float32 ) )
     self._l_t0      = tf.Variable( 0., dtype = tf.float32 )
     self._l_t1      = tf.Variable( 0., dtype = tf.float32 )
@@ -101,9 +114,12 @@ class AIS(object):
                  , parallel_iterations=1
                  , swap_memory=True )
     # Unflatten final results
-    desired_shape = ( self._nchains, self._l_const_ndata )
-    final_state = tf.reshape( self._l_z, desired_shape + (self._generator_wrapper.latent_dim(),) )
-    lower_bound_logp = tf.reshape( self._l_w, desired_shape )
+    unflattened_latent = self.unflatten(self._l_z)
+    if self._l_const_condition is not None:
+      unflattened_condition = tuple( self.unflatten(c) for c in self._l_const_condition )
+    # FIXME assumes that condition is a tuple/list, needs to implement method for dict
+    final_state = self._generator_wrapper.build_generator_input( unflattened_condition, unflattened_latent )
+    lower_bound_logp = self.unflatten( self._l_w )
     # Compute statistics
     avg_lower_bound_logp = np_avg_logexp(lower_bound_logp, axis = 0)
     final_lld = np.mean(avg_lower_bound_logp)
@@ -120,7 +136,16 @@ class AIS(object):
     forward_nchains = self._nchains
     self._nchains = 1
     # Add noise to x
-    x = tf.add(x, tf.random.normal( x.shape, stddev=tf.sqrt(self._observation_model.var) ) )
+    condition = self._generator_wrapper.extract_condition_from_generator_input( x )
+    target = self._generator_wrapper.extract_target_space_from_data( x )
+    x = self._generator_wrapper.build_input( 
+        condition
+      , tf.add(target, tf.random.normal( target.shape, stddev=tf.sqrt(self._observation_model.var) ) )
+    )
+    if isinstance(z, (tuple,list)):
+      z = tuple(tf.expand_dims( sz, axis = 0 ) for sz in z) # unflatten
+    else:
+      z = tf.expand_dims( z, axis = 0 ) # unflatten
     out = self.forward_ais(x, z, schedule)
     self._nchains = forward_nchains
     return out
@@ -134,21 +159,39 @@ class AIS(object):
   def _energy_fn(self, z, t = None):
     if t is None: t = self._l_t1
     prior = self._generator_wrapper.latent_log_prob(z)
+    # FIXME check if this implementation is correctly aligned for the condition
+    # inputs and outputs 
     posterior = tf.multiply( t,  
       # Flatten observation results
-      tf.reshape( 
+      self.flatten(  
         self._observation_model(
           self._l_const_x, 
           # Unflatten generated data so that observation model
           # can broadcast operations on the input space
-          tf.reshape( self._generator_wrapper.transform(z)
-            , ( self._nchains, self._l_const_ndata, ) + self._l_const_data_dim
+          self.unflatten(
+            self._generator_wrapper.extract_target_space_from_data(
+              self._generator_wrapper.transform(
+                self._generator_wrapper.build_generator_input(self._l_const_condition, z)
+              )
+            )
           )
-        ), ( self._nchains * self._l_const_ndata, )
+        ), 
       )
     )
     energy = tf.multiply( -1.0, tf.add( prior, posterior ) )
     return energy
+
+  @tf.function
+  def unflatten(self, data):
+    return tf.reshape( data
+        , [ self._nchains, self._l_const_ndata, ] + data.shape[1:].as_list()
+    )
+
+  @tf.function
+  def flatten(self, data):
+    return tf.reshape( data
+      , [ self._nchains * self._l_const_ndata, ] + data.shape[2:].as_list()
+    )
 
   @tf.function
   def _update_w(self): 
@@ -171,6 +214,7 @@ class AIS(object):
     index = tf.add(index,1)
     return (index,)
 
+  @tf.function
   def _condition(self, *args):
     index = args[0]
     return tf.less(index, self._l_const_len_schedule)
